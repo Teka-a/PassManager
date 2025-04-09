3f5c9fa8022e7a9a611e80fdd37deefa68093992
Teka-a <katya-osipova-1999@mail.ru>
2025-04-09 20:43:16 +0300

    tests changed

--- test/tst_file.cpp
+++ test/tst_file.cpp
@@ -67,6 +67,40 @@ TEST_F(CryptoTests, encryptStr2)
 }
 
 
+TEST_F(CryptoTests, decryptStr1)
+{
+    QString textToDec = "7dfbec05754fb8b96c294212ac7571ac9ca55bc77a67ca1ee36de634903d6454";
+    QByteArray ciphertext = QByteArray::fromHex(textToDec.toUtf8());
+
+    QByteArray producedPlaintext;
+    if (!decryptAES256CBC(ciphertext, *key, *iv, producedPlaintext)) {
+        qDebug() << "Error during decryption occured!";
+    }
+
+    QString expectedStr = "very important text";
+    QByteArray expectedPlaintext = expectedStr.toUtf8();
+
+    EXPECT_EQ(expectedPlaintext, producedPlaintext);
+}
+
+
+TEST_F(CryptoTests, decryptStr2)
+{
+    QString textToDec = "3fb3e4682f3a581c7565afe9d79f8b2bcdaa12e580c4c0532a8a62490f0e6caf";
+    QByteArray ciphertext = QByteArray::fromHex(textToDec.toUtf8());
+
+    QByteArray producedPlaintext;
+    if (!decryptAES256CBC(ciphertext, *key, *iv, producedPlaintext)) {
+        qDebug() << "Error during decryption occured!";
+    }
+
+    QString expectedStr = "not really important text";
+    QByteArray expectedPlaintext = expectedStr.toUtf8();
+
+    EXPECT_EQ(expectedPlaintext, producedPlaintext);
+}
+
+
 TEST_F(CryptoTests, deriveKeyInfo)
 {
     QString salt = "../../info.db";
@@ -83,46 +117,3 @@ TEST_F(CryptoTests, deriveKeyInfo)
     EXPECT_EQ(expected, produced);
 }
 
-/*
-TEST(deriveKeyInfo, file)
-{
-    QString salt = "../../info.db";
-    QString pin = "12345";
-    QByteArray key;
-    QByteArray iv;
-
-    QString derivedKey = "b340947333ac4783f8d88683acc45f0165834e14160df964ffa5d3aee6b9c8dc";
-    QString derivedIV = "65834e14160df964ffa5d3aee6b9c8dc";
-
-    computeAESKeyAndIV(pin.toUtf8(), salt.toUtf8(), key, iv);
-
-    QPair<QByteArray, QByteArray> produced {key, iv};
-    QPair<QByteArray, QByteArray> expected {QByteArray::fromHex(derivedKey.toUtf8()), QByteArray::fromHex(derivedIV.toUtf8())};
-
-    EXPECT_EQ(expected, produced);
-}
-
-
-
-
-TEST(encStr, file)
-{
-    QString textToEnc = "very important text";
-    QByteArray plaintext = textToEnc.toUtf8();
-
-    QString derivedKey = "b340947333ac4783f8d88683acc45f0165834e14160df964ffa5d3aee6b9c8dc";
-    QString derivedIV = "65834e14160df964ffa5d3aee6b9c8dc";
-    QByteArray key = QByteArray::fromHex(derivedKey.toUtf8());
-    QByteArray iv = QByteArray::fromHex(derivedIV.toUtf8());
-
-    QByteArray producedCiphertext;
-    if (!encryptAES256CBC(plaintext, key, iv, producedCiphertext)) {
-        qDebug() << "Error during encryption occured!";
-    }
-
-    QString expectedStr = "7dfbec05754fb8b96c294212ac7571ac9ca55bc77a67ca1ee36de634903d6454";
-    QByteArray expectedCiphertext = QByteArray::fromHex(expectedStr.toUtf8());
-
-    EXPECT_EQ(expectedCiphertext, producedCiphertext);
-}
-*/
